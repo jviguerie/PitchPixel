697e1b24506031e10bb38264ec5d2476d6a8d166
jviguerie <52045567+jviguerie@users.noreply.github.com>
2019-06-20 18:40:09 -0700

    test batch
    test batch

---
+++ G6_test_all.bat
@@ -0,0 +1,4 @@
+REM Test Video 0
+G6_tgt_tracking.py -v ColorCorr_Video_0_Tre_.mp4 -o G6_0.avi -f 30 -c MPEG
+REM Test Video 1
+G6_tgt_tracking.py -v ColorCorr_Video_1_Seb_.mp4 -o G6_1.avi -f 30 -c MPEG
