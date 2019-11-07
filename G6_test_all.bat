3f176795cf61ad809dc45e04b9fff22d86367a05
jviguerie <52045567+jviguerie@users.noreply.github.com>
2019-11-06 17:19:47 -0800

    Delete G6_test_all.bat

--- G6_test_all.bat
+++
@@ -1,4 +0,0 @@
-REM Test Video 0
-G6_tgt_tracking.py -v ColorCorr_Video_0_Tre_.mp4 -o G6_0.avi -f 30 -c MPEG
-REM Test Video 1
-G6_tgt_tracking.py -v ColorCorr_Video_1_Seb_.mp4 -o G6_1.avi -f 30 -c MPEG
